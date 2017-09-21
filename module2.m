198221e9e29c53165809d4ca8ee97457258aee06
Amanda Szymanski <ltsamanda@yahoo.com>
2017-09-21 11:39:35 -0400

    Update module2.m

--- module2.m
+++ module2.m
@@ -1,7 +1,3 @@
-% devi's training: module 2
-% this will import connectomes & show how to manipulate them to show only specific portions based on location or thresholding.
-
-
 
 % import a specific connectome
 connectome_import = sprintf('/Volumes/Cabeza/MemEX.01/Data/SHIBA_INU/35014/DWI/Processing/connectome_35014_471_seed_image_NOACT.csv');
@@ -70,5 +66,55 @@ full_connectome_100(1:size(full_connectome_100,1)+1:end) = diag(connectome_100);
 % and one more visualization
 imagesc(full_connectome_100);colorbar
 
-ok, 
+% you can see how the connectome changes visually when the diagonal values
+% are doubled versus at their original values
 
+% ok, time to assign proper labels to each ROI. we need to import the hoa_100 label file.
+
+filename = '/Volumes/Cabeza/MemEX.01/Data/Matlab_devi/hoa_100_labels.mat';
+load(filename);
+hoa_100_labels = hoa_100;
+
+% we can't actually apply the labels directly to the connectome file and do
+% subsequent visualization because the value type changes from "double" to
+% "cell", so we're just importing the hoa labels and will refer back to
+% them.
+
+% create a variable called 'order' that goes from 1 to 85 (the number of
+% labels in the hoa_100 file)
+
+order = [1:85]';
+
+% now create a variable that contains only the first column of the full
+% connectome
+connectome_100_col1 = full_connectome_100([1:85], 1);
+
+% put order & connectome_100_col1 into one variable
+ordered_connectome = [order,connectome_100_col1];
+
+% if you plot it you can see the variation of streamlines
+plot(ordered_connectome)
+
+% now you can sort ordered_connectome from highest to lowest number of
+% streamlines. -2 sorts it by the second column & it sorts it in descending
+% order
+sorted_ordered_connectome = sortrows(ordered_connectome, -2);
+
+% plot it
+plot(sorted_ordered_connectome)
+
+% now that the connectome is sorted by streamline value, we can look at the
+% number assigned to each streamline value & crosscheck it with the
+% hoa_100_labels to see which ROIs have the largest number of streamlines.
+
+% we can also create a cell that will contain streamline values associated
+% with specific regions.
+
+% first, turn ordered_connectome into a cell
+ordered_connectome_cell = num2cell(ordered_connectome);
+
+% then combine hoa_100_labels with connectome_100_cell
+connectome_100_labeled = [connectome_100_col1test,hoa_100_labels];
+
+% now you can easily see which numeric value is associated with each region
+% & the number of associated streamlines
